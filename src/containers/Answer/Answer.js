b10237f1dd5aca5f94cb9a3ede858fda45742282
momojunzi <mark.holthaus@gmail.com>
2018-04-22 21:39:27 -0700

    ok

--- src/containers/Answer/Answer.js
+++ src/containers/Answer/Answer.js
@@ -16,7 +16,7 @@ class Answer extends Component {
 
   getAnswer = () => {
     const server = process.env.REACT_APP_API_URL || '';
-    axios.get('http://localhost:8080/api/answer')
+    axios.get('http://0.0.0.0:8080/api/answer')
       .then(result =>{
         console.log(result);
         const answer = result.data;
